f830e05685e1dc35a66bc1243a74b9c55bc1d639
Sree1701 <ksnavasree@gmail.com>
2025-09-17 12:22:13 +0530

    Done

---
+++ data_type.py
@@ -0,0 +1,11 @@
+num_int =int(input("Enter an integer:"))
+print("Integer value: ",num_int)
+print("Type of number is :", type(num_int))
+
+num_float = float(input("Enter an float:"))
+print("\nFloat value:", num_float)
+print("Type of number is:", type(num_float))
+
+num_complex =complex(input("Enter an complex:"))
+print("\nComplex value :", num_complex)
+print("Type of numbers is:",type(num_complex))
---
+++ string_repeat.py
@@ -0,0 +1,4 @@
+string=str(input("Enter a string:"))
+n=int(input("Enter a number:"))
+print("String copies:\n",(string+'\n')*n)
+
