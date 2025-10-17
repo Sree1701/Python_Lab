bb7c9adcaa8240cb0bf702d5089d50867e9dca30
Sree1701 <ksnavasree@gmail.com>
2025-10-17 10:19:23 +0530

    done

--- merge_dict.py
+++ merge_dict.py
@@ -1,6 +1,6 @@
 dict1={}
 dict1[int(input("Enter Key for dict1:"))]=input("Enter value:")
 dict2={}
-dict2[int(input("Enter key for dict1:"))]=input("Enter value:")
+dict2[int(input("Enter key for dict2:"))]=input("Enter value:")
 dict1.update(dict2)
 print("Merged dictionary:",dict1)
